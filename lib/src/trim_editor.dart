4fe2c1cc4479503c5716231294cbf7d7f3abd4c8
Khaled Mortaja <79102102+khaledmurtaja@users.noreply.github.com>
2022-08-28 18:29:07 +0300

    Update trim_editor.dart

--- lib/src/trim_editor.dart
+++ lib/src/trim_editor.dart
@@ -311,7 +311,7 @@ class _TrimEditorState extends State<TrimEditor> with TickerProviderStateMixin {
     _numberOfThumbnails = widget.viewerWidth ~/ _thumbnailViewerH;
 
     _thumbnailViewerW = _numberOfThumbnails * _thumbnailViewerH;
-    _startPos =  Offset(0.111*_thumbnailViewerW,0);
+    _startPos =  Offset(0.238*_thumbnailViewerW,0);
 //     _endPos =  Offset(0.333*_thumbnailViewerW,_thumbnailViewerH);
 //     _videoStartPos = 20.0;//45*( _startPos.dx/ _thumbnailViewerW);
 //     _videoEndPos = 40.0;
