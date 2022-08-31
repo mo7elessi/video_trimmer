633e766b805dfc242546ef0b34738a6c659e9a90
Khaled Mortaja <79102102+khaledmurtaja@users.noreply.github.com>
2022-08-31 13:18:30 +0300

    Update videoFlag.dart

--- lib/src/videoFlag.dart
+++ lib/src/videoFlag.dart
@@ -1,1 +1,12 @@
 
+class VideoFlag{
+  int? flagPoint;
+  
+  int? BeforeFlag=5;
+  ///by default==> 5 sec before the flag time by , and that's why its an optional param
+  
+  int? afterFlag=10;
+  /// by default==>10 after the flag time  and that's why its an optional param
+
+  VideoFlag({ required this.flagPoint,this.afterFlag,this.BeforeFlag});
+}
