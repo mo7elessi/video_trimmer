b32678a9a918af4084a54984bcfcccb7ae3ef733
Khaled Mortaja <79102102+khaledmurtaja@users.noreply.github.com>
2022-09-01 17:19:08 +0300

    Update trim_editor.dart

--- lib/src/trim_editor.dart
+++ lib/src/trim_editor.dart
@@ -207,7 +207,7 @@ class _TrimEditorState extends State<TrimEditor> with TickerProviderStateMixin {
   double _videoStartPos = 0.0;
   double _videoEndPos = 0.0;
 
-  Offset _startPos = const Offset(900, 0);
+  Offset _startPos = const Offset(0, 0);
   Offset _endPos = const Offset(0, 0);
 
   double _startFraction = 0.0;
