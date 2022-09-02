6fb4266e5377ceacd23d6c974ed2a141bec16300
Khaled Mortaja <79102102+khaledmurtaja@users.noreply.github.com>
2022-09-02 22:07:49 +0300

    Update trim_editor.dart

--- lib/src/trim_editor.dart
+++ lib/src/trim_editor.dart
@@ -591,6 +591,7 @@ class _TrimEditorState extends State<TrimEditor> with TickerProviderStateMixin {
     );
   }
 }
+}
 
 enum EditorDragType {
   /// The user is dragging the left part of the frame.
