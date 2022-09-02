906ec690545cb3039ba52a6a3e428881f0af17c4
Khaled Mortaja <79102102+khaledmurtaja@users.noreply.github.com>
2022-09-02 21:32:49 +0300

    Update trim_editor.dart

--- lib/src/trim_editor.dart
+++ lib/src/trim_editor.dart
@@ -319,6 +319,9 @@ class _TrimEditorState extends State<TrimEditor> with TickerProviderStateMixin {
      if(videoFlagList!=null){
        
         Future.delayed(Duration(milliseconds: 100), () {  
+          print("fsdfjksdlfjksjkldjlk");
+          print(_durationx);
+           print("fsdfjksdlfjksjkldjlk");
      VideoFlag flag= _validateFlagPoint(videoFlagList!.elementAt(0),_durationx);
        _startPos =  Offset((flag.BeforeFlag!/_durationx)*_thumbnailViewerW,0);
    
