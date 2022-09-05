7954ebb2b4f2805ab426449fe12a6094a0f354c5
Khaled Mortaja <79102102+khaledmurtaja@users.noreply.github.com>
2022-09-05 19:26:20 +0300

    Update trim_editor.dart

--- lib/src/trim_editor.dart
+++ lib/src/trim_editor.dart
@@ -328,12 +328,10 @@ class _TrimEditorState extends State<TrimEditor> with TickerProviderStateMixin {
        _endPos =  Offset(flag.afterFlag!/videoDuration!*_thumbnailViewerW,_thumbnailViewerH);
        _videoStartPos = flag.BeforeFlag!*1000;
       _videoEndPos = flag.afterFlag!*1000;
-       print(_videoStartPos.toInt());
-        videoPlayerController
-            .seekTo(Duration(milliseconds: _videoStartPos.toInt()));
        
-   
- 
+
+       
+
     
   }
   }
