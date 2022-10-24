1891f6dd8c5149867097704e3a243460fab0e5ae
Khaled Mortaja <79102102+khaledmurtaja@users.noreply.github.com>
2022-10-24 12:06:09 +0300

    Update trim_editor.dart

--- lib/src/trim_editor.dart
+++ lib/src/trim_editor.dart
@@ -602,6 +602,7 @@ if(widget.flagModel == null){
         child:InkWell(
           onLongPress:(){
                 TrimmerPage.deletingMode.value=true;
+                TrimmerPage.deletingMode.notifyListeners();
             print(" success lol lolo");
           },
           child:Container(
